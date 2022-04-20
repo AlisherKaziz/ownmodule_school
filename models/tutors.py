0ce62eef83ab533769942920f869b04a54fece48
Alisher Kaziz <alisher.kaziz@gmail.com>
2022-04-20 16:25:56 +0600

    v6

--- models/tutors.py
+++ models/tutors.py
@@ -5,10 +5,15 @@ class SchoolTutors(models.Model):
     _name = "school.tutor"
     _description = "Tutor Table"
 
+    @api.depends('name', 'surname', 'secondname')
+    def _compute_fullname(self):
+        for record in self:
+            record.fullname = "%s %s %s" % record.surname, record.name, record.secondname
+
     name = fields.Char(string="Name", required=True)
     surname = fields.Char(string="Surname", required=True)
     secondname = fields.Char(string="SecondName", required=True)
-    age = fields.Integer(string="Age")
+    fullname = fields.Char(compute='_compute_fullname')
     specialization = fields.Char(string="Specialization")
     subject_description = fields.Text(string="Subject Description")
     gender = fields.Selection([
