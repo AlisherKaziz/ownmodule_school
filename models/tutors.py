0e3dfa1cc0715238daaee6803a3fc2b1b43040d6
Alisher Kaziz <alisher.kaziz@gmail.com>
2022-04-27 16:07:58 +0600

    v10

--- models/tutors.py
+++ models/tutors.py
@@ -5,17 +5,18 @@ class SchoolTutors(models.Model):
     _name = "school.tutor"
     _description = "Tutor Table"
 
-    @api.depends('name', 'surname')
-    def compute_fullname(self):
+    @api.depends('name', 'surname', 'fullname')
+    def _compute_fullname(self):
         self.fullname = "{} {}".format(self.name, self.surname)
+        pass
 
     name = fields.Char(string="Name", required="True")
     surname = fields.Char(string="Surname", required="True")
-    fullname = fields.Char(strring="Fullname", compute="compute_fullname")
+    fullname = fields.Char(string="Fullname", compute="_compute_fullname")
     subject_description = fields.Text(string="Subject Description")
     gender = fields.Selection([
-        ('male', 'Male'),
-        ('female', 'Female'),
-        ('other', 'Other')
-    ], string='Gender', default='other')
+        ('1', 'Male'),
+        ('2', 'Female'),
+        ('3', 'Other')
+    ], string='Gender', default='3')
     image = fields.Binary(string='Image')
